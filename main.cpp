469a502dfca11e4b02522eed24ede36893ec270f
Mad Vitaliy <omelchenko.vitaliy1@gmail.com>
2022-02-20 12:57:07 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Created by Vitaliy Omelchenko 20.02.22
+//Lab2: Pro
+/*
+ * Created by Vitaliy Omelchenko 20.02.22
+ * Lab2: Problem-solving with search
+ *
+ * +--+---------+---------+-------+-----+
+ * |# |Task     |AlgNoInf |AlgInf |Func |
+ * +--+---------+---------+-------+-----+
+ * |4 |8-puzzle |LDFS     |RBFS   |H1   |
+ * +--+---------+---------+-------+-----+
+ */
+#include <iostream>
+
+int main()
+  {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+  }
